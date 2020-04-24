a832121d1c3fea30b032c39de7d482a64b2c8f50
Andreas Frei <arfrei@hotmail.com>
2020-04-24 17:59:04 +0200

    hardcode redis/mongo

--- src/Conductor/EnvironmentVariables.cs
+++ src/Conductor/EnvironmentVariables.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 namespace Conductor
 {
     public static class EnvironmentVariables
-    {        
-        public static string DbHost => Environment.GetEnvironmentVariable("dbhost");
-        public static string Redis => Environment.GetEnvironmentVariable("redis");
+    {
+        public static string DbHost => "mongodb+srv://wfuser:logo12Q2@pdf4mecluster-zhvoy.azure.mongodb.net/test?retryWrites=true&w=majority"; // Environment.GetEnvironmentVariable("dbhost");
+        public static string Redis => "dmscacheq1.redis.cache.windows.net:6380,password=qKccuJsH6l+vy14KQdvy3DRLPLnLPL2NaTPJsjH3+Gg=,ssl=True,abortConnect=False"; // Environment.GetEnvironmentVariable("redis");
         public static string Auth => Environment.GetEnvironmentVariable("auth");
         public static string PublicKey => Environment.GetEnvironmentVariable("publickey");
         public static string Alg => Environment.GetEnvironmentVariable("alg");
