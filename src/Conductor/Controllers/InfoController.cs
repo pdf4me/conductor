d88fc8a0b7fcb708893b8ce4ea34a2d2877b0fe3
Chandra Arora <cp.arora@hotmail.com>
2020-04-27 07:56:41 +0200

    added env

--- src/Conductor/Controllers/InfoController.cs
+++ src/Conductor/Controllers/InfoController.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Conductor.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
@@ -15,11 +16,13 @@ namespace Conductor.Controllers
     [ApiController]    
     public class InfoController : ControllerBase
     {
-        private IConfiguration configuration; 
+        private IConfiguration configuration;
+        private IWebHostEnvironment env;
 
-        public InfoController(IConfiguration configuration)
+        public InfoController(IConfiguration configuration, IWebHostEnvironment env)
         {
             this.configuration = configuration;
+            this.env = env;
         }
         [HttpGet]        
         public ActionResult<DiagnosticInfo> Get()
@@ -34,8 +37,8 @@ namespace Conductor.Controllers
                 WorkingSet = process.WorkingSet64,
                 Version = version.ProductVersion,
                 OSVersion = Environment.OSVersion.VersionString,
-                Enviornment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
-                BuildId = Environment.GetEnvironmentVariable("Build_Id")
+                Enviornment = $" EnvironmentName {env.EnvironmentName} ASPNETCORE_ENVIRONMENT {Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}" , //Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+                BuildId =  Environment.GetEnvironmentVariable("Build_Id")
 
             };
         }
